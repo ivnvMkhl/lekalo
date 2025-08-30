a0b5211cd21246af7c3b31381e5e34fbd1b9e94f
iMkhl <mkhl.ivnv@gmail.com>
2025-08-30 14:31:08 +0300

    Add multiple values support

--- core/generate.go
+++ core/generate.go
@@ -3,13 +3,14 @@ package core
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/ivnvMkhl/lekalo/config"
 	"github.com/ivnvMkhl/lekalo/locales"
 	"github.com/ivnvMkhl/lekalo/render"
 )
 
-func GenerateTemplate(templateName string, userInputs map[string]string) error {
+func GenerateTemplate(templateName string, userInputs map[string]interface{}) error {
 	cfg, err := config.LoadConfigs()
 	if err != nil {
 		return err
@@ -20,100 +21,86 @@ func GenerateTemplate(templateName string, userInputs map[string]string) error {
 		return fmt.Errorf("%s '%s' %s", locales.T("common", "template"), templateName, locales.T("common", "not_found"))
 	}
 
-	params := make(map[string]interface{})
-	for _, param := range template.Params {
-		value, ok := userInputs[param.Name]
-		if !ok {
-			value = param.Default
-		}
-		params[param.Name] = value
-	}
+	params := prepareTemplateParams(template.Params, userInputs)
 
 	if template.Folders != nil {
-		for _, dirPath := range template.Folders {
+		for folderName, dirPath := range template.Folders {
 			resolvedPath, err := render.ResolvePath(dirPath, params)
 			if err != nil {
-				return err
+				return fmt.Errorf("%s %s: %w", locales.T("generate", "path_resolve_error"), folderName, err)
 			}
-			if err := render.EnsureDir(resolvedPath); err != nil {
-				return err
+			if err := os.MkdirAll(resolvedPath, 0755); err != nil {
+				return fmt.Errorf("%s %s: %w", locales.T("generate", "folder_create_error"), resolvedPath, err)
 			}
 		}
 	}
 
-	for _, file := range template.Files {
-		filePath, err := render.ResolvePath(file.Path, params)
-		if err != nil {
-			return err
+	for fileName, file := range template.Files {
+		if err := processFile(file, params); err != nil {
+			return fmt.Errorf("%s %s: %w", locales.T("generate", "file_processed_error"), fileName, err)
 		}
-
-		content, err := render.RenderString(file.Template, params)
-		if err != nil {
-			return err
-		}
-
-		switch file.Mode {
-		case "always_overwrite":
-			{
-				if err := overwriteFile(filePath, content); err != nil {
-					return err
-				}
-			}
-		case "skip_if_exist":
-			{
-				f, err := os.Open(filePath)
-				if err != nil {
-					if err := writeFile(filePath, content); err != nil {
-						return err
-					}
-				} else {
-					fmt.Printf("%s: %s\n", locales.T("generate", "file_skip"), filePath)
-				}
-				f.Close()
-			}
-		case "append":
-			{
-				if err := appendToFile(filePath, content); err != nil {
-					return err
-				}
-			}
-		default:
-			{
-				f, err := os.Open(filePath)
-				if err == nil {
-					input := UserInput(fmt.Sprintf("%s %s", filePath, locales.T("generate", "rewrite_message")))
-					if confirm := UserInputToBool(input); confirm {
-						if err := overwriteFile(filePath, content); err != nil {
-							return err
-						}
-					}
-				} else {
-					if err := writeFile(filePath, content); err != nil {
-						return err
-					}
-				}
-				f.Close()
-			}
-		}
-
 	}
 
 	return nil
 }
 
-func writeFile(path string, content string) error {
-	if err := render.EnsureDir(path); err != nil {
-		return err
+func processFile(file config.FileTemplate, params map[string]interface{}) error {
+	filePath, err := render.ResolvePath(file.Path, params)
+	if err != nil {
+		return fmt.Errorf("%s %s: %w", locales.T("generate", "path_resolve_error"), file.Path, err)
 	}
+
+	content, err := render.RenderString(file.Template, params)
+	if err != nil {
+		return fmt.Errorf("%s: %w", locales.T("generate", "render_error"), err)
+	}
+
+	switch file.Mode {
+	case "always_overwrite":
+		return overwriteFile(filePath, content)
+	case "skip_if_exist":
+		return handleSkipIfExist(filePath, content)
+	case "append":
+		return appendToFile(filePath, content)
+	default:
+		return handleInteractive(filePath, content)
+	}
+}
+
+func handleSkipIfExist(filePath, content string) error {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return writeFile(filePath, content)
+	}
+	fmt.Printf("%s: %s\n", locales.T("generate", "file_skip"), filePath)
+	return nil
+}
+
+func handleInteractive(filePath, content string) error {
+	if _, err := os.Stat(filePath); err == nil {
+		input := UserInput(fmt.Sprintf("%s %s", filePath, locales.T("generate", "rewrite_message")))
+		if UserInputToBool(input) {
+			return overwriteFile(filePath, content)
+		}
+		return nil
+	}
+	return writeFile(filePath, content)
+}
+
+func writeFile(path string, content string) error {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("%s %s: %w", locales.T("generate", "folder_create_error"), dir, err)
+	}
+
 	if err := os.WriteFile(path, []byte(content), 0644); err != nil {
-		return err
+		return fmt.Errorf("%s %s: %w", locales.T("generate", "file_write_error"), path, err)
 	}
 	fmt.Printf("%s: %s\n", locales.T("common", "file_created"), path)
 	return nil
 }
 
 func overwriteFile(path string, content string) error {
-	if err := os.WriteFile(path, []byte(content), 0644); err != nil {
+	if err := writeFile(path, content); err != nil {
 		return err
 	}
 	fmt.Printf("%s: %s\n", locales.T("generate", "file_rewrited"), path)
@@ -121,6 +108,11 @@ func overwriteFile(path string, content string) error {
 }
 
 func appendToFile(path string, content string) error {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("%s %s: %w", locales.T("generate", "folder_create_error"), dir, err)
+	}
+
 	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return fmt.Errorf("%s: %w", locales.T("generate", "file_open_failed"), err)
